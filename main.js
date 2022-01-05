a9f3cabec3fff29a11ab48f7d40de00a1e25872b
JahnieTurner <80420943+JahnieTurner@users.noreply.github.com>
2022-01-04 19:39:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX = 0;
+noseY = 0;
+
 function preload() {
 
 }
@@ -6,13 +9,27 @@ function setup() {
     canvas = createCanvas(400, 400)
     canvas.center()
     video = createCapture(VIDEO)
-
+    video.hide()
+    posenet = ml5.poseNet(video, modelLoaded)
+    posenet.on('pose', gotposes)
 }
 
 function draw() {
-
+    image(video, 0, 0, 400, 400)
 }
 
 function takesnapshot() {
     save('mustashe.png')
+}
+
+function modelLoaded() {
+    console.log("model is loaded")
+}
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results)
+        noseX = results[0].pose.nose.x
+        noseX = results[0].pose.nose.x
+    }
 }
